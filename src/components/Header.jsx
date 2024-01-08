cbb3ed28057d51d5ed72eee423edadcc67ecd2e0
leslientodi <lesliehazard500@gmail.com>
2024-01-08 12:07:39 +0000

    completed header

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header (){
+    return (
+        <div className="header">
+            <img src="../images/troll.jpeg"></img>
+            <h1>MemeGenerator</h1>
+            <small>React Course - Project 3</small>
+        </div>
+    )
+}
---
+++ src/components/Meme.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Meme() {
+    return (
+        <form>
+            <input></input>
+            <input></input>
+        </form>
+    )
+}
