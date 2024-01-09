a13c68ec2b4f6ba3e6f5c4b31c8252dfcd5464ab
leslientodi <lesliehazard500@gmail.com>
2024-01-09 13:00:00 +0000

    completed button rendering with usestate and random memes

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 
+
 export default function Meme() {
+    let memeImage = ['../images/meme1.jpeg', '../images/meme2.jpeg', '../images/meme3.jpeg']
+    const [image, setMeme] = React.useState("")
+    function newMeme () {
+        const memeOut = memeImage[Math.floor(Math.random()*memeImage.length)]
+         setMeme(memeOut)
+    }
     return (
-        <form>
-            <input className="input-1"></input>
-            <input className="input-2"></input><br></br>
-            <button>Get a new meme image 🖼️</button>
-        </form>
+        <>
+            <form>
+                <input className="input-1"></input>
+                <input className="input-2"></input><br></br>
+                
+            </form>
+            <button className="button" onClick={newMeme}>Get a new meme🖼️</button>
+            <img className="meme-image" src={image}></img>
+        </>
+        
+        
     )
 }
